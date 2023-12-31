f9af0f55ea1d18ddbf1ab2a01beef34ad8263203
AdhirathKumar <adhirathkdg@gmail.com>
2024-01-01 01:25:51 +0530

    Changed the code so that it no longer uses Fernet as it is less secure

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pickle
-import os
-import base64
-from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import base64  # Added for base64 encoding
 
 # ------------------------- Original to Encrypted -------------------------
 
@@ -19,13 +19,16 @@ if uploaded_file is not None:
 
     salt = os.urandom(16)  # Generate a random salt
     kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=390000)
-    key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
+    key = base64.urlsafe_b64encode(kdf.derive(password.encode()))[:32]  # Extract 32 bytes
 
-    f = Fernet(key)
-    encrypted_data = f.encrypt(image_data)
+    backend = default_backend()
+    iv = os.urandom(16)  # Generate a random initialization vector (IV)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    encryptor = cipher.encryptor()
+    encrypted_data = encryptor.update(image_data) + encryptor.finalize()
 
     with open("encrypted_image.pickle", "wb") as f:
-        pickle.dump((encrypted_data, salt), f)
+        pickle.dump((encrypted_data, iv, salt), f)  # Store IV and salt
 
     st.success("Image encrypted successfully!")
 
@@ -46,15 +49,22 @@ encrypted_file = st.file_uploader("Upload an encrypted image (.bin format)")
 
 if encrypted_file is not None:
     with open("encrypted_image.pickle", "rb") as f:
-        encrypted_data_from_file, salt = pickle.load(f)
+        encrypted_data_from_file, iv, salt = pickle.load(f)
 
-    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=390000)
-    key_from_file = base64.urlsafe_b64encode(kdf.derive(password_verify.encode()))
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=390000,
+        backend=backend
+    )
+    key_from_file = base64.urlsafe_b64encode(kdf.derive(password_verify.encode()))[:32]
 
-    f = Fernet(key_from_file)
+    cipher = Cipher(algorithms.AES(key_from_file), modes.CBC(iv), backend=backend)
+    decryptor = cipher.decryptor()
 
     try:
-        decrypted_data = f.decrypt(encrypted_data_from_file)
+        decrypted_data = decryptor.update(encrypted_data_from_file) + decryptor.finalize()
 
         st.success("Image decrypted successfully!")
         st.image(decrypted_data, caption="Decrypted Image")
