6e15c9ab06b3d1f1eff32ba339c6257c87bda470
AdhirathKumar <adhirathkdg@gmail.com>
2024-01-01 13:25:00 +0530

    Fixed the padding issue.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import os
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.padding import PKCS7
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
@@ -28,7 +29,9 @@ if uploaded_file is not None:
     iv = os.urandom(16)  # Generate a random initialization vector (IV)
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
     encryptor = cipher.encryptor()
-    encrypted_data = encryptor.update(image_data) + encryptor.finalize()
+    padder = PKCS7(algorithms.AES.block_size).padder()
+    padded_data = padder.update(image_data) + padder.finalize()
+    encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
 
     with open("encrypted_image.pickle", "wb") as f:
         pickle.dump((encrypted_data, iv, salt), f)  # Store IV and salt
@@ -53,6 +56,7 @@ password_verify = st.text_input(
 encrypted_file = st.file_uploader("Upload an encrypted image (.bin format)")
 
 if encrypted_file is not None:
+    backend = default_backend()
     with open("encrypted_image.pickle", "rb") as f:
         encrypted_data_from_file, iv, salt = pickle.load(f)
 
@@ -64,15 +68,13 @@ if encrypted_file is not None:
         backend=backend,
     )
     key_from_file = base64.urlsafe_b64encode(kdf.derive(password_verify.encode()))[:32]
-
     cipher = Cipher(algorithms.AES(key_from_file), modes.CBC(iv), backend=backend)
     decryptor = cipher.decryptor()
+    decrypted_data = decryptor.update(encrypted_data_from_file) + decryptor.finalize()
+    unpadder = PKCS7(algorithms.AES.block_size).unpadder()
+    unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
 
     try:
-        decrypted_data = (
-            decryptor.update(encrypted_data_from_file) + decryptor.finalize()
-        )
-
         st.success("Image decrypted successfully!")
         st.image(decrypted_data, caption="Decrypted Image")
     except:
